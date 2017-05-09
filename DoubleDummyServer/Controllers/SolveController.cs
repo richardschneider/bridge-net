6f8c32b14195a5198d5095ea01cb1ff52792c9ae
Richard Schneider <makaretu@gmail.com>
2017-05-10 10:52:32 +1200

    feat(solve): web page for solving

---
+++ DoubleDummyServer/Controllers/SolveController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoubleDummyServer.Controllers
+{
+    public class SolveController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Double Dummy Solver";
+
+            return View();
+        }
+    }
+}
