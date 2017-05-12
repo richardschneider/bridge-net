582c81fa82d52a10ecfe1be28de24071441431d4
Richard Schneider <makaretu@gmail.com>
2017-05-12 19:25:28 +1200

    docs(dds): API overview #2

---
+++ DoubleDummyServer/Controllers/DocsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoubleDummyServer.Controllers
+{
+    public class DocsController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "DDS API Overview";
+
+            return View();
+        }
+
+        public ActionResult Api(string id)
+        {
+            ViewBag.Title = "DDS API";
+            return View(id);
+        }
+    }
+}
