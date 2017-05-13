ad6aca23fcc2a2a0276419d5a879943919280171
Richard Schneider <makaretu@gmail.com>
2017-05-13 14:00:29 +1200

    docs(dds): remove VS api cruft #2

--- DoubleDummyServer/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace DoubleDummyServer.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- DoubleDummyServer/Global.asax.cs
+++ DoubleDummyServer/Global.asax.cs
@@ -13,7 +13,6 @@ namespace DoubleDummyServer
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
